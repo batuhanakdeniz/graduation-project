e955ed6f3c2ea04a53ebb9fc8f37ec5406fc305a
sefaAydogan <69217207+sefaAydogan@users.noreply.github.com>
2021-04-11 21:44:37 +0300

    Log out, detail page, navbar loggedIn changed

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getUser, getUserProfile, createUser, loginUser, getloggedIn } from "../controllers/userController.js";
+import { getUser, getUserProfile, createUser, loginUser, getloggedIn, getloggedOut } from "../controllers/userController.js";
 import { auth } from "../middleware/auth.js";
 
 const router = express.Router();
@@ -8,6 +8,7 @@ router.get('/', getUser);
 router.post('/signup/:userType', createUser);
 router.post('/login', loginUser);
 router.get('/loggedIn', getloggedIn);
+router.get('/loggedOut', getloggedOut);
 router.get('/profile/:id', getUserProfile);
 
 export default router;
