7308752249088413fe9b3e639f61eaabd51788f8
Muhammed Batuhan Akdeniz <49098686+batuhanakdeniz@users.noreply.github.com>
2021-04-25 13:29:47 +0300

    Server-API

--- server/routers/helpRouter.js
+++ server/routers/helpRouter.js
@@ -1,15 +1,15 @@
 import express from "express";
-import { getHelp, createHelp } from "../controllers/helpController.js";
+import {getHelp, createHelp,getHelpLocations,getHelpLocation,getHelpBasics,getHelpBasic, getHelpDetail, getHelpDetails} from '../controllers/helpController.js'
 import {auth} from "../middleware/auth.js";
 const router = express.Router();
 
 //router.get('/', getHelp);
-router.post('/yardimekle/',auth ,createHelp);
+router.post('/api/help',createHelp);
 router.get('/get/',auth ,getHelp);
-router.get('api/helps/location');
-router.get('api/helps/location/:id')
-router.get('api/helps/basic');
-router.get('api/helps/basic/:id');
-router.get('api/helps/detail');
-router.get('api/helps/detail/:id');
+router.get('api/helps/locations', getHelpLocations);
+router.get('api/helps/locations/:id',getHelpLocation)
+router.get('api/helps/basics',getHelpBasics);
+router.get('api/helps/basics/:id',getHelpBasic);
+router.get('api/helps/details',getHelpDetail);
+router.get('api/helps/details/:id',getHelpDetails);
 export default router;
